efce613ba0a9ccb52e700b24b867fb8b85c61d91
Hyeseong212 <86096770+Hyeseong212@users.noreply.github.com>
2024-07-01 02:04:16 +0900

    상대방 움직임 보간처리

--- Assets/Scripts/InGame/Controller/CharacterTrController.cs
+++ Assets/Scripts/InGame/Controller/CharacterTrController.cs
@@ -27,15 +27,16 @@ public class CharacterTrController : MonoBehaviour
         }
     }
 
-    //모든 유저들의 캐릭터들을 동기화시켜줌
     [SerializeField] List<GameObject> Characters = new List<GameObject>();
     public Dictionary<long, GameObject> UserTr = new Dictionary<long, GameObject>();
+    private Dictionary<long, Vector3> targetPositions = new Dictionary<long, Vector3>();
+    private Dictionary<long, Quaternion> targetRotations = new Dictionary<long, Quaternion>();
     GameObject ThisUserCharacter;
+    public float lerpSpeed = 20f;
 
-    public void Init()//여기엔 기본적인 Transform값만
+    public void Init()
     {
-        Characters.Clear(); // 기존 캐릭터 리스트 초기화
-        // Players 태그가 달린 모든 게임 오브젝트를 가져와서 Characters 리스트에 추가
+        Characters.Clear();
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
         foreach (GameObject player in players)
         {
@@ -53,17 +54,72 @@ public class CharacterTrController : MonoBehaviour
 
     public void FixedUpdate()
     {
-        if (InGameSessionController.Instance.inGameSessionInfo.isPlayerInfoOK && ThisUserCharacter != null)// 서버에 동기화완료되면 캐릭터 위치 뿌려줘
+        if (InGameSessionController.Instance.inGameSessionInfo.isPlayerInfoOK && ThisUserCharacter != null)
         {
+            LerpingOpponentMove();
             SendClientCharacterTr(ThisUserCharacter.transform.localPosition, ThisUserCharacter.transform.localRotation);
         }
     }
 
+    public void UpdatePlayerTR(byte[] data)
+    {
+        long userUID = BitConverter.ToInt64(data, 0);
+        int playerNum = BitConverter.ToInt32(data, 8);
+        if (!UserTr.ContainsKey(userUID))
+        {
+            UserTr.Add(userUID, Characters[playerNum - 1]);
+        }
+        else
+        {
+            if (userUID != Global.Instance.standbyInfo.userEntity.UserUID)
+            {
+                float posX = BitConverter.ToSingle(data, 12);
+                float posY = BitConverter.ToSingle(data, 16);
+                float posZ = BitConverter.ToSingle(data, 20);
+
+                float rotX = BitConverter.ToSingle(data, 24);
+                float rotY = BitConverter.ToSingle(data, 28);
+                float rotZ = BitConverter.ToSingle(data, 32);
+                float rotW = BitConverter.ToSingle(data, 36);
+
+                Vector3 newPosition = new Vector3(posX, posY, posZ);
+                Quaternion newRotation = new Quaternion(rotX, rotY, rotZ, rotW);
+
+                targetPositions[userUID] = newPosition;
+                targetRotations[userUID] = newRotation;
+            }
+        }
+    }
+    private void LerpingOpponentMove()
+    {
+        foreach (var kvp in UserTr)
+        {
+            long userId = kvp.Key;
+            GameObject character = kvp.Value;
+
+            if (character != ThisUserCharacter)
+            {
+                if (targetPositions.TryGetValue(userId, out Vector3 targetPosition) &&
+                    targetRotations.TryGetValue(userId, out Quaternion targetRotation))
+                {
+                    character.transform.localPosition = Vector3.Lerp(
+                        character.transform.localPosition,
+                        targetPosition,
+                        lerpSpeed * Time.deltaTime
+                    );
+
+                    character.transform.localRotation = Quaternion.Slerp(
+                        character.transform.localRotation,
+                        targetRotation,
+                        lerpSpeed * Time.deltaTime
+                    );
+                }
+            }
+        }
+    }
     private void SendClientCharacterTr(Vector3 clientChTr, Quaternion quaternion)
     {
-        //유저 캐릭터 위치값 보내기 유저UID + 유저 캐릭터위치
         Packet packet = new Packet();
-
         int length = 0x01 + 0x01 + Utils.GetLength(Global.Instance.standbyInfo.userEntity.UserUID) + Utils.GetLength(clientChTr.x) + Utils.GetLength(clientChTr.y) + Utils.GetLength(clientChTr.z)
             + Utils.GetLength(quaternion.x) + Utils.GetLength(quaternion.y) + Utils.GetLength(quaternion.z) + Utils.GetLength(quaternion.w);
 
@@ -87,41 +143,6 @@ public class CharacterTrController : MonoBehaviour
         {
             byte[] trData = data.Skip(1).ToArray();
             UpdatePlayerTR(trData);
-        }
-    }
-    public void UpdatePlayerTR(byte[] data)//업데이트 시켜주고
-    {
-        long userUID = BitConverter.ToInt64(data, 0);
-        int playerNum = BitConverter.ToInt32(data, 8);
-        if (!UserTr.ContainsKey(userUID))
-        {
-            UserTr.Add(userUID, Characters[playerNum - 1]);
-        }
-        else
-        {
-            InGameTCPController.Instance.EnqueueDispatcher(() =>
-            {
-                // 현재 사용자의 UID가 아니면 위치와 회전을 업데이트
-                if (userUID != Global.Instance.standbyInfo.userEntity.UserUID)
-                {
-                    float posX = BitConverter.ToSingle(data, 12);
-                    float posY = BitConverter.ToSingle(data, 16);
-                    float posZ = BitConverter.ToSingle(data, 20);
-
-                    float rotX = BitConverter.ToSingle(data, 24);
-                    float rotY = BitConverter.ToSingle(data, 28);
-                    float rotZ = BitConverter.ToSingle(data, 32);
-                    float rotW = BitConverter.ToSingle(data, 36);
-
-                    GameObject playerObject;
-                    if (UserTr.TryGetValue(userUID, out playerObject))
-                    {
-                        // 해당 GameObject의 위치와 회전을 업데이트
-                        playerObject.transform.localPosition = new Vector3(posX, posY, posZ);
-                        playerObject.transform.localRotation = new Quaternion(rotX, rotY, rotZ, rotW);
-                    }
-                }
-            });
         }
     }
 }
